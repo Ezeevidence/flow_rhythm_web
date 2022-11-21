cdadc1e7dfa4a47a2b3a8b257323c8dfc68393d7
Ezeevidence <eze.pleasant234@gmail.com>
2022-11-21 13:07:40 +0100

    update

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -5,7 +5,6 @@ import 'package:flowrhythm_web/widgets/countdown.dart';
 import 'package:flowrhythm_web/widgets/email_form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import 'widgets/social_buttons.dart';
 
@@ -48,16 +47,16 @@ class _LandingPageState extends State<LandingPage> {
               image: AssetImage(path("background.png")), fit: BoxFit.cover),
         ),
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(50, 15, 0, 0),
+          padding: const EdgeInsets.fromLTRB(50, 15, 0, 20),
           child: SingleChildScrollView(
-            controller: scrollController,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Center(
-                  child: SvgPicture.asset("logos.svg"),
-                ),
+                    child: Image(
+                  image: AssetImage(path("logo.png")),
+                )),
                 SizedBox(
                   height: getHeight(33),
                 ),
@@ -71,7 +70,7 @@ class _LandingPageState extends State<LandingPage> {
                 ),
                 const EmailContainer(),
                 SizedBox(
-                  height: getHeight(124),
+                  height: getHeight(100),
                 ),
                 const SocialButtons(),
                 SizedBox(
