4f531f5f2a273a2b96ae61fc34da021c7d25a779
Ezeevidence <eze.pleasant234@gmail.com>
2022-11-25 10:16:54 +0100

    updates

--- lib/widgets/email_form_field.dart
+++ lib/widgets/email_form_field.dart
@@ -12,55 +12,64 @@ class EmailContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        SizedBox(
-          height: getHeight(72, context),
-          width: getWidth(545, context),
-          // decoration: BoxDecoration(
-          //     border: Border.all(
-          //       color: Colors.white,
-          //     ),
-          //     borderRadius: const BorderRadius.all(Radius.circular(20))),
-          child: TextFormField(
-            controller: emailController,
-            cursorColor: Colors.white,
-            style:
-                TextStyle(color: Colors.white, fontSize: getFont(20, context)),
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.white.withOpacity(0.26),
-              border: const OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-              contentPadding:
-                  const EdgeInsets.only(left: 20, bottom: 5, top: 5, right: 20),
-              hintText: "Enter email address",
-              hintStyle: TextStyle(
+    return LayoutBuilder(builder: (context, constraints) {
+      return Stack(
+        children: [
+          SizedBox(
+            height: getHeight(72, context),
+            width: getWidth(545, context),
+            // decoration: BoxDecoration(
+            //     border: Border.all(
+            //       color: Colors.white,
+            //     ),
+            //     borderRadius: const BorderRadius.all(Radius.circular(20))),
+            child: TextFormField(
+              controller: emailController,
+              cursorColor: Colors.white,
+              style: TextStyle(
                   color: Colors.white, fontSize: getFont(20, context)),
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.26),
+                border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                contentPadding: const EdgeInsets.only(
+                    left: 20, bottom: 5, top: 5, right: 20),
+                hintText: "Enter email address",
+                hintStyle: TextStyle(
+                    color: Colors.white, fontSize: getFont(20, context)),
+              ),
             ),
           ),
-        ),
-        Positioned(
-            right: 0,
-            child: SizedBox(
-              height: getHeight(68, context),
-              width: getWidth(170, context),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: mainGreen,
+          Positioned(
+              right: 0,
+              child: SizedBox(
+                height: getHeight(68, context),
+                width: getWidth(170, context),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(mainGreen),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            side: BorderSide(color: Colors.white)),
+                      )),
+                  onPressed: () async {
+                    uploadEmail();
+                    DialogBox(
+                      context: context,
+                      textEditingController: emailController,
+                    ).dialogBox(context);
+                  },
+                  child: const Text("Join Wait list"),
                 ),
-                onPressed: () async {
-                  uploadEmail();
-                  DialogBox(
-                    context: context,
-                    textEditingController: emailController,
-                  ).dialogBox(context);
-                },
-                child: const Text("Join Wait list"),
-              ),
-            ))
-      ],
-    );
+              ))
+        ],
+      );
+    });
   }
 
   Future<void> uploadEmail() async {
